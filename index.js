552caf80b0f7a05110103f22b30e1eda40b28cea
Sharon Watkins <swatkins876@gmail.com>
2020-04-10 12:47:56 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(record) {
+  let year
+  record.find(obj => {
+    if (obj["result"] === "W") {
+      year = obj.year
+    }
+  })
+  return year
+}
